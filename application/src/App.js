6c619df2cf91044d7309755cce1378df821a6772
mfsalomao <mfsalomao@gmail.com>
2020-08-22 20:49:17 +0200

    segundo commit

---
+++ application/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let x = 100;
+  let y =  setInterval(() => {
+     x --;
+  },5000);
+
+
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Martinho ama Monique {x}</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
